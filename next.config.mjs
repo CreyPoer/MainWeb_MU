4714a04999dab2b9abc72c4eaf9c026966acb406
CreyPoer <sonione04@gmail.com>
2026-02-19 11:10:23 +0700

    refactor: Migrate Next.js configuration from TypeScript to ES Module JavaScript format.

---
+++ next.config.mjs
@@ -0,0 +1,101 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    output: 'standalone',
+    typescript: {
+        ignoreBuildErrors: true,
+    },
+    eslint: {
+        ignoreDuringBuilds: true,
+    },
+    images: {
+        qualities: [75, 100],
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'admin-mu.maduraunitedfc.com',
+                port: '',
+                pathname: '/storage/**',
+            },
+            {
+                protocol: 'https',
+                hostname: 'admin-mu.maduraunitedfc.id',
+                port: '',
+                pathname: '/**',
+            },
+            {
+                protocol: 'https',
+                hostname: 'i.ytimg.com',
+                port: '',
+                pathname: '/**',
+            },
+            {
+                protocol: 'https',
+                hostname: 'flagcdn.com',
+                port: '',
+                pathname: '/**',
+            },
+            {
+                protocol: 'https',
+                hostname: 'maduraunitedfc.com',
+                port: '',
+                pathname: '/**',
+            },
+            {
+                protocol: 'https',
+                hostname: 'upload.wikimedia.org',
+                port: '',
+                pathname: '/**',
+            },
+            {
+                protocol: 'https',
+                hostname: 'img.youtube.com',
+                port: '',
+                pathname: '/**',
+            },
+            {
+                protocol: 'https',
+                hostname: 'images.unsplash.com',
+                port: '',
+                pathname: '/**',
+            },
+            {
+                protocol: 'https',
+                hostname: 'img.freepik.com',
+                port: '',
+                pathname: '/**',
+            },
+            {
+                protocol: 'http',
+                hostname: 'localhost',
+                port: '8000',
+                pathname: '/**',
+            },
+            {
+                protocol: 'http',
+                hostname: '127.0.0.1',
+                port: '8000',
+                pathname: '/**',
+            },
+            {
+                protocol: 'https',
+                hostname: 'localhost',
+                port: '8000',
+                pathname: '/**',
+            },
+            {
+                protocol: 'https',
+                hostname: '127.0.0.1',
+                port: '8000',
+                pathname: '/**',
+            },
+            {
+                protocol: 'https',
+                hostname: 'images.pexels.com',
+                port: '',
+                pathname: '/**',
+            },
+        ],
+    },
+};
+
+export default nextConfig;
